7b28b9176d43db3bb08fc28bfaa8c2b516891aff
Palmxl <jmartin_sanchez@javeriana.edu.co>
2026-05-08 00:08:48 -0500

    feat: connect frontend with backend ticket API

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.database.connection import engine
 from app.database.base import Base
 
@@ -8,6 +10,14 @@ from app.routes.ticket_routes import router as ticket_router
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(ticket_router)
 
